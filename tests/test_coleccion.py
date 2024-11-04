4b4cd51a9931f34b6c5e10fd44b7e534e99590a5
Fredy Alarcon <f.alarconf@uniandes.edu.co>
2024-11-03 22:33:46 -0500

    amplia cobertura

--- tests/test_coleccion.py
+++ tests/test_coleccion.py
@@ -4,6 +4,7 @@ from src.logica.coleccion import Coleccion
 from src.modelo.album import Album
 from src.modelo.cancion import Cancion
 from src.modelo.interprete import Interprete
+from src.modelo.album import Album
 from src.modelo.declarative_base import Session, Base, engine
 from unittest.mock import patch
 
@@ -20,9 +21,29 @@ class ColeccionTestCase(unittest.TestCase):
         self.interprete_prueba = Interprete(nombre="Intérprete de Prueba")
         self.session.add(self.interprete_prueba)
         self.session.commit()
+        # Agregar un álbum de prueba
+        self.album_prueba = Album(titulo="Álbum de Prueba")
+        self.session.add(self.album_prueba)
+        self.session.commit()
+        # Agregar una canción de prueba
+        self.cancion_prueba = Cancion(titulo="Canción de Prueba")
+        self.session.add(self.cancion_prueba)
+        self.session.commit()
 
     def test_eliminar_interprete_excepcion(self):
         with self.assertRaises(Exception) as context:
             self.coleccion.eliminar_interprete(9999)  # ID que sabemos que no existe
        
         self.assertEqual(str(context.exception), "Intérprete no encontrado.")
+        
+    def test_eliminar_album_excepcion(self):
+        with self.assertRaises(Exception) as context:
+            self.coleccion.eliminar_album(9999)  # ID que sabemos que no existe
+        
+        self.assertEqual(str(context.exception), "Álbum no encontrado.")
+        
+    def test_eliminar_cancion_excepcion(self):
+        with self.assertRaises(Exception) as context:
+            self.coleccion.eliminar_cancion(9999)
+            
+        self.assertEqual(str(context.exception), "Canción no encontrada.")
