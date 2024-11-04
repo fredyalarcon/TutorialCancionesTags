562e0155f190659ddb64c2b45afcc7f2bd35afc8
Fredy Alarcon <f.alarconf@uniandes.edu.co>
2024-11-03 19:08:49 -0500

    Ajuste pipeline y retsauración issue

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -18,6 +18,16 @@ class Coleccion():
             return True
         else:
             return False
+        
+    def agregar_album_copiasonar(self, titulo, anio, descripcion, medio):
+        busqueda = session.query(Album).filter(Album.titulo == titulo).all()
+        if len(busqueda) == 0:
+            album = Album(titulo=titulo, ano=anio, descripcion=descripcion, medio=medio)
+            session.add(album)
+            session.commit()
+            return True
+        else:
+            return False
 
     def dar_medios(self):
         return [medio.name for medio in Medio]
